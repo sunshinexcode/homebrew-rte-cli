42afc42948c7a20a02d252aef9adf0eb6267de15
lixinhui <lixinhui@agora.io>
2024-05-27 11:24:04 +0800

    chore(): init

---
+++ Formula/rte-cli.rb
@@ -0,0 +1,29 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class BuzzCli < Formula
+  desc 'RTE CLI'
+  homepage ''
+  url 'https://github.com/sunshinexcode/homebrew-rte-cli/releases/download/v0.0.1/buzz-cli-macos-x64.tar.gz'
+  sha256 '190a295f9f43c0dfcf9dcdc506309cc647e0cd2c3561b5c80f9c2b857bb46618'
+  license 'MIT'
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install 'rte-cli'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tpc`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system 'false'
+  end
+end
---
+++ README.md
@@ -0,0 +1,26 @@
+# homebrew-rte-cli
+
+```sh
+$ brew tap sunshinexcode/homebrew-rte-cli
+```
+
+## rte-cli
+
+### Install
+
+```sh
+$ brew tap sunshinexcode/homebrew-rte-cli
+$ brew install rte-cli
+```
+
+或者
+
+```sh
+$ brew install sunshinexcode/rte-cli/rte-cli
+```
+
+### upgrade
+
+```sh
+$ brew upgrade rte-cli
+```
